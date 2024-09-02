ddaf3b7955748e4a58132f45d0f6ca0f41767021
Baylee Kimmel <bkimmel1226@gmail.com>
2024-09-02 15:03:50 -0400

    Basic functionality, animation created, different views created

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,34 @@
-console.log('I AM CONNECTED')
+document.addEventListener('DOMContentLoaded', () => {
+    const main = document.getElementById('mainContainer')
+    const projects = document.getElementById('projects')
+    const aboutMe = document.getElementById('aboutMe')
+
+    main.classList.add('animate-fade-in')
+
+    document.getElementById('projectsBtn').addEventListener('click', () => {
+        switchSection(main, projects)
+    })
+
+    document.getElementById('aboutMeBtn').addEventListener('click', () => {
+        switchSection(main, aboutMe)
+    })
+
+    document.getElementById('projectsToHomeBtn').addEventListener('click', () => {
+        switchSection(projects, main)
+    })
+
+    document.getElementById('aboutMeToHomeBtn').addEventListener('click', () => {
+        switchSection(aboutMe, main)
+    })
+
+    const switchSection = (hideSection, showSection) => {
+        hideSection.style.display = 'none'
+        showSection.style.display = 'flex'
+        const sectionContent = showSection.querySelector('section')
+        sectionContent.style.display = 'block'
+        sectionContent.classList.add('animate-fade-in')
+        sectionContent.addEventListener('animationend', () => {
+            sectionContent.classList.remove('animate-fade-in')
+        }, { once: true })
+    }
+})
